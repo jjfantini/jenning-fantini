0f9f166bbf3adb6d791042eea2de962d32d71191
jenningsFantini <jenningsfantini+github@gmail.com>
2025-02-20 15:39:03 -0500

    refactor(layout): adjust main content layout and remove margin

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -6,7 +6,7 @@ import { RepellingText } from "./_components/repelling-title";
 
 export default function LandingPage() {
   return (
-    <main className="min-h-screen flex flex-col items-center p-4 gap-8 mt-10">
+    <main className="min-h-screen flex flex-col items-center justify-center p-4 gap-8">
       <RepellingText 
         text="Jennings Fantini"
         className="md:text-5xl text-4xl lg:text-7xl font-bold text-neutral-900 dark:text-neutral-100"
