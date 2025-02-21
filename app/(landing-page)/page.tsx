57b271485e5a91185cdc2e237c659f06300251f4
jenningsFantini <jenningsfantini+github@gmail.com>
2025-02-21 10:55:14 -0500

    refactor(landing-page): optimize text layout and typography

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,5 +1,3 @@
-// 'use client'
-
 import { TerminalIntro } from "./_components/terminal-intro";
 import { AuroraText } from "@/components/ui/aurora-text";
 import { RepellingText } from "./_components/repelling-title";
@@ -7,17 +5,19 @@ import { RepellingText } from "./_components/repelling-title";
 export default function LandingPage() {
   return (
     <div className="flex flex-col items-center justify-center p-4 gap-8">
-      <RepellingText 
-        text="Jennings Fantini"
-        className="md:text-5xl text-4xl lg:text-7xl font-bold text-neutral-900 dark:text-neutral-100"
-      />
-      <div className="max-w-4xl">
+      <span>
+        <RepellingText 
+          text="Jennings Fantini"
+          className="max-w-4xl text-4xl md:text-6xl lg:text-8xl font-bold text-neutral-900 dark:text-neutral-100"
+          />
+      </span>
+      <span className="max-w-4xl">
         <TerminalIntro />
-      </div>
+      </span>
 
-        <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
-          <AuroraText>create.</AuroraText>
-        </h1>
+      <span className="font-bold tracking-tighter text-4xl md:text-6xl lg:text-7xl">
+        <p><AuroraText>execute.</AuroraText></p>
+      </span>
     </div>
   );
 }
